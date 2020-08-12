db5fcb08bacc136389afcf595a7cc465f3255613
ferdibulus <53481851+ferdibulus@users.noreply.github.com>
2020-08-12 05:23:08 +0300

    first

---
+++ spring-rest-demo/src/main/java/com/ferdi/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.ferdi.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.ferdi.springdemo")
+public class DemoAppConfig {
+
+}
---
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring Rest</title>
+</head>
+<body>
+<h3>Spring REST Demo Version @author ferdibulus</h3>
+<br>
+<br>
+
+<a href="/test/hello">Hello Rest</a>
+</body>
+</html>
